08185bd6534eec7add145007d7819948d015aeed
Jelte Boelens <j.w.r.boelens@gmail.com>
2017-05-06 23:37:53 +0200

    Added a redirect so that you won't get resubmits anymore

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
@@ -21,14 +21,15 @@ def fibonacci(amount):
         fib.append(fib[-1]+fib[-2])
     return fib[amount]
 
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NumberForm()
-    number = 0
     if form.validate_on_submit():
-        number = fibonacci(form.number.data)
-        form.number.data = None
-    return render_template("index.html", number=number, form=form)
+        session["number"] = fibonacci(form.number.data)
+        return redirect(url_for("index"))
+    return render_template("index.html", number=session.get("number"), form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
