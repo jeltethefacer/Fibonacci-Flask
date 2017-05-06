ca324419301b7e2f0a561898853c7bba1d651d49
Jelte Boelens <j.w.r.boelens@gmail.com>
2017-05-06 19:47:54 +0200

    Added a Form to request the user the fibonacci number he wants

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dit raad je toch nooit in jouw missearbele leven"
+
 bootstrap = Bootstrap(app)
+
+
+
+class NumberForm(FlaskForm):
+    number = IntegerField("Which fibonacci number?", validators=[Required()])
+    submit = SubmitField("Submit")
 
 def fibonacci(amount):
     fib = [0,1]
@@ -10,9 +21,14 @@ def fibonacci(amount):
         fib.append(fib[-1]+fib[-2])
     return fib[amount]
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", number=fibonacci(10))
+    form = NumberForm()
+    number = 0
+    if form.validate_on_submit():
+        number = fibonacci(form.number.data)
+        form.number.data = None
+    return render_template("index.html", number=number, form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
