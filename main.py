480e90e4efd29ec252728fd529624c30d8bcdf15
Jelte Boelens <j.w.r.boelens@gmail.com>
2017-05-06 17:23:05 +0200

    added function for fibonacci number and display it in the webpage

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+def fibonacci(amount):
+    fib = [0,1]
+    for x in range(amount -1):
+        fib.append(fib[-1]+fib[-2])
+    return fib[amount]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", number=fibonacci(10))
 
 if __name__ == "__main__":
     app.run(debug=True)
